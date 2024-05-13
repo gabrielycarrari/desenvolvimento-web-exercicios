5de7e681abc84160eb8d7875958292c2175c9aad
Gabriely Carrari <gabymcarrari@gmail.com>
2024-05-13 13:32:31 -0300

    :recycle: refactor: correção ex.5.8

--- Exercícios Livro Python/cap 5/ex.5.8.py
+++ Exercícios Livro Python/cap 5/ex.5.8.py
@@ -2,8 +2,11 @@ def decifrar_mensagem(msg: str):
     if len(msg) == 0:
         return msg
     else:
-        caracter_dec = chr((ord(msg[0])- 98) % 26 + 97)
-        return caracter_dec + decifrar_mensagem(msg[1:])
+        if msg[0] == " ":
+            return " " + decifrar_mensagem(msg[1:])
+        else: 
+            caracter_dec = chr((ord(msg[0])- 98) % 26 + 97)
+            return caracter_dec + decifrar_mensagem(msg[1:])
     
 
 mensagem_dec = decifrar_mensagem("hbcz")
